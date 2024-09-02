15e8f6c6bf9da14b9abe28b0bf8dd7f3cb3c894a
tyskie97 <tyskie97@gmail.com>
2024-09-03 00:51:52 +0200

    Maly update

--- systemPrep.sh
+++ systemPrep.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
-sudo yum install docker
+sudo yum -y install docker >/dev/null 2>&1 &

6b722f0cc72d9a540f989821205bf0b7cc7821dc
tyskie97 <tyskie97@gmail.com>
2024-09-03 00:58:32 +0200

    Aktualizacja systemu po instalacji dockera

--- systemPrep.sh
+++ systemPrep.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
-sudo yum -y install docker >/dev/null 2>&1 &
+sudo yum -y install docker # >/dev/null 2>&1 & --instalacja po cichu
+sudo yum -y update

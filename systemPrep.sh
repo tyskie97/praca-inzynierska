b5780aaed192bb2d271a5f7d72866aada2b6f4f1
tyskie97 <tyskie97@gmail.com>
2024-09-14 21:37:36 +0200

    Fix instalacji dockera

--- systemPrep.sh
+++ systemPrep.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
-sudo yum -y install docker # >/dev/null 2>&1 & --instalacja po cichu
-sudo yum -y update
+yum install -y yum-utils # >/dev/null 2>&1 & --instalacja po cichu
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y update

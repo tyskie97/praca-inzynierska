dd6d6563cf8b9b9f572b5076b2a4878dadd5f9c2
tyskie97 <tyskie97@gmail.com>
2024-09-14 21:57:07 +0200

    Dodanie flag do komend

--- systemPrep.sh
+++ systemPrep.sh
@@ -2,6 +2,18 @@
 
 echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
 yum install -y yum-utils # >/dev/null 2>&1 & --instalacja po cichu
+echo ">>>>>>>>>=================
+INSTALACJA YUM-UTILIS
+>>>>>>>>>================="
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+echo ">>>>>>>>============
+DODANIE REPO
+>>>>>>>=============="
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo ">>>>>>>>>>>================
+INSTALACJE PACZEK DOCKERA
+>>>>>>>>>>>==============="
 yum -y update
+echo ">>>>>>>>>================
+UPDATE SYSTEMU
+>>>>>>>>>>>==============="

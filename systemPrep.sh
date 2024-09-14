d8656f58fdc774a94b3203dfda7f12ee8a1e071d
tyskie97 <tyskie97@gmail.com>
2024-09-14 22:10:13 +0200

    Enable i wlaczenie docker service

--- systemPrep.sh
+++ systemPrep.sh
@@ -3,17 +3,22 @@
 echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
 yum install -y yum-utils # >/dev/null 2>&1 & --instalacja po cichu
 echo ">>>>>>>>>=================
-INSTALACJA YUM-UTILIS
+AFTER INSTALACJA YUM-UTILIS
 >>>>>>>>>================="
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 echo ">>>>>>>>============
-DODANIE REPO
+AFTER DODANIE REPO
 >>>>>>>=============="
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo ">>>>>>>>>>>================
-INSTALACJE PACZEK DOCKERA
+AFTER INSTALACJE PACZEK DOCKERA
 >>>>>>>>>>>==============="
 yum -y update
 echo ">>>>>>>>>================
-UPDATE SYSTEMU
+AFTER UPDATE SYSTEMU
 >>>>>>>>>>>==============="
+systemctl start docker.service
+systemctl enable docker.service
+echo ">>>>>>>>>>============
+AFTER WLACZENIE I ENABLE DOCKER SERVICE
+>>>>>>>>>>>>============"

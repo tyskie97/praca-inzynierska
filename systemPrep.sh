8d50193dfe3bcdbbcbc0a793a7741082ca56723e
tyskie97 <tyskie97@gmail.com>
2024-09-14 21:51:06 +0200

    Install dockera

--- systemPrep.sh
+++ systemPrep.sh
@@ -3,4 +3,5 @@
 echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
 yum install -y yum-utils # >/dev/null 2>&1 & --instalacja po cichu
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 yum -y update

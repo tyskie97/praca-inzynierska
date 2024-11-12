637b94e4121d951fa41cc3e79857fd53b5dd08b5
tyskie97 <tyskie97@gmail.com>
2024-11-12 10:15:23 +0100

    Clean up script and added creation of directories for Jira and psql

--- systemPrep.sh
+++ systemPrep.sh
@@ -1,24 +1,22 @@
 #! /bin/bash
 
-echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych.Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
+echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych. Skrypt najlepiej wykonac z konta root lub z kontem z rownie wysokimi uprawnieniami."
+echo "Instaluje yum-utils."
 yum install -y yum-utils # >/dev/null 2>&1 & --instalacja po cichu
-echo ">>>>>>>>>=================
-AFTER INSTALACJA YUM-UTILIS
->>>>>>>>>================="
+echo "Dodaje repozytorium."
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-echo ">>>>>>>>============
-AFTER DODANIE REPO
->>>>>>>=============="
+echo "Instaluje wszystkie paczki wymagane do zainstalowania Dockera."
 yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-echo ">>>>>>>>>>>================
-AFTER INSTALACJE PACZEK DOCKERA
->>>>>>>>>>>==============="
+echo "Aktualizazja wszystkich paczek w systemie."
 yum -y update
-echo ">>>>>>>>>================
-AFTER UPDATE SYSTEMU
->>>>>>>>>>>==============="
+echo "Wlaczam service Docker-a oraz ustawiam automatyczne wlaczenie wraz z wystartowaniem systemu."
 systemctl start docker.service
 systemctl enable docker.service
-echo ">>>>>>>>>>============
-AFTER WLACZENIE I ENABLE DOCKER SERVICE
->>>>>>>>>>>>============"
+echo "Tworze foldery w ktorych beda przechowywane pliki aplikacji dla Jiry oraz dla PostgreSQL
+Jira przechowywana jest w /srv/docker/jira
+PostgreSQL przechowywany jest w /srv/docker/psql"
+mkdir /srv/docker/psql/data
+mkdir /srv/docker/jira/data 
+echo "Przygotowania systemu zakonczone, wykonuje skrypt przygotowania bazy danych."
+
+./setUpDataBase.sh

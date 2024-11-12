647c8783338b01b5a7ea41cedf5146978e403cba
tyskie97 <tyskie97@gmail.com>
2024-11-12 23:37:54 +0100

    Using printf instead of echos and fixing typos and formating

--- systemPrep.sh
+++ systemPrep.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "Witaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych. Skrypt najlepiej wykonac z konta root lub z konta z rownie wysokimi uprawnieniami."
-echo "Instaluje yum-utils."
+printf "\nWitaj w skrypcie automatyzujacym przygotowanie systemu i deploy Jiry oraz bazy danych. Skrypt najlepiej wykonac z konta root lub z konta z rownie wysokimi uprawnieniami."
+printf "\nInstaluje yum-utils."
 show_loading() { 				# petla ktorej zadaniem jest uswiadomienie uzytkownika o wciaz dzialajacym procesie
         while true;
         do
@@ -9,9 +9,9 @@ show_loading() { 				# petla ktorej zadaniem jest uswiadomienie uzytkownika o wc
                 for i in {1..5};
                 do
                         printf "."
-                        sleep 0.3
+                        sleep 0.5
                 done
-        printf "\r            \r"
+        printf "\r                                                \r"
         done
 }
 show_loading &          			# wykonanie komendy w tle 
@@ -19,30 +19,30 @@ show_loading_PID=$!     			# pobranie numeru procesu ostatniej komendy w tym prz
 yum install -y yum-utils  >/dev/null 2>&1 	# instalacja po cichu
 kill "$show_loading_PID" 2>/dev/null
 
-echo "Dodaje repozytorium."
+printf "\nDodaje repozytorium."
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-echo "Instaluje wszystkie paczki wymagane do zainstalowania Dockera."
+printf "\nInstaluje wszystkie paczki wymagane do zainstalowania Dockera."
 show_loading &
 show_loading_PID=$!
 yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
 kill "$show_lading_PID" 2>/dev/null
 
-echo "Aktualizazja wszystkich paczek w systemie."
+printf "\nAktualizacja wszystkich paczek w systemie."
 show_loading &
 show_loading_PID=$!
 yum -y update >/dev/null 2>&1
 kill "$show_loading_PID" 2>/dev/null
 
-echo "Wlaczam service Docker-a oraz ustawiam automatyczne wlaczenie wraz z wystartowaniem systemu."
+printf "\nWlaczam service Docker-a oraz ustawiam automatyczne wlaczenie wraz z wystartowaniem systemu."
 systemctl start docker.service
 systemctl enable docker.service
 
-echo "Tworze foldery w ktorych beda przechowywane pliki aplikacji dla Jiry oraz dla PostgreSQL
+printf "\nTworze foldery w ktorych beda przechowywane pliki aplikacji dla Jiry oraz dla PostgreSQL
 Jira przechowywana jest w /srv/docker/jira
 PostgreSQL przechowywany jest w /srv/docker/psql"
 mkdir -p /srv/docker/psql/data			# flaga -p pozwala na tworzenie zagniezdzonych folderow
 mkdir -p /srv/docker/jira/data 
 
-echo "Przygotowania systemu zakonczone, wykonuje skrypt przygotowania bazy danych."
+printf "\nPrzygotowania systemu zakonczone, wykonuje skrypt przygotowania bazy danych."
 ./setUpDataBase.sh

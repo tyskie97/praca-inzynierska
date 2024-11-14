112e18526755fcd5057bb6203f4aaf9625464238
tyskie97 <tyskie97@gmail.com>
2024-11-14 14:35:16 +0100

    just to count low long it takes

---
+++ howLong.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+seconds=57
+minutes=0
+hours=0 			#god pls stay at 0
+
+
+while true;
+do
+	while [ "$seconds" -le 59 ];
+	do
+		if [ "$minutes" -le 9 ];
+		then
+			if [ "$seconds" -le 9 ];
+			then
+				printf "\r0$minutes:0$seconds"
+			else
+				printf "\r0$minutes:$seconds"
+			fi
+		else
+                        if [ "$seconds" -le 10 ];
+                        then
+                                printf "\r$minutes:0$seconds"
+                        else
+                                printf "\r$minutes:$seconds"
+                        fi
+
+		fi
+		seconds=$((seconds + 1))
+		sleep 1
+		printf "\r     \r"
+	done
+	minutes=$((minutes + 1))
+	seconds=0
+done

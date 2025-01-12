abc2ea96e23b4f84925a308c872c5b033ceb8447
tyskie97 <tyskie97@gmail.com>
2025-01-12 02:36:42 +0100

    Templates for compose sripts for jira and psql

--- setUpDataBase.sh
+++ setUpDataBase.sh
@@ -1,3 +1,30 @@
 #! /bin/bash
+version: '3.5'
+
+services:
+  postgres_16_4:
+    container_name: postgres_16_4
+    image: postgres:16.4
+    networks:
+      - postgres16_4
+    environment:
+      POSTGRES_USER: postgres
+      POSTGRES_PASSWORD: <HASLO>
+    volumes:
+       - <CALA_SCIEZKA>/data:/var/lib/postgresql/data
+       - <CALA_SCIEZKA_BACKUP>:/data/backup
+    ports:
+      - "<PORTY:PORTY>"
+    restart: always
+
+networks:
+  postgres16_4:
+    name: postgres16_4
+    driver: bridge
+
+### Usuwanie katalogów i plików starszych niz 14 dni dla backupu baz
+30 01 * * * find /mnt/backup/postgres/11.13 -type d -mtime +13 | xargs rm -rf
 
 
+DO BACKUPU KOMENDA
+30 02 * * * docker exec -d -u postgres postgres11_13 bash -c "pg_basebackup -D /var/lib/postgresql/data/bck-$(date +\%Y\%m\%d-\%H-\%M-\%S) -F t -z -c fast"
--- setUpJira.sh
+++ setUpJira.sh
@@ -1,1 +1,36 @@
 #! /bin/bash
+
+version: '3'
+services:
+  jira:
+    container_name: jira_8_22
+    image: atlassian/jira-software:8.22.0 # wczesniej atlassian/jira-software:8.22
+    #networks:
+    # - postgres11_13
+    environment:
+     - 'JVM_MINIMUM_MEMORY=3072m'
+     - 'JVM_MAXIMUM_MEMORY=16G'
+     - 'ATL_PROXY_NAME=jira.envelo.pl'
+     - 'ATL_PROXY_PORT=443'
+     - 'ATL_TOMCAT_SCHEME=https'
+     - 'ATL_TOMCAT_SECURE=true'
+    extra_hosts:
+     - "jira.envelo.pl:10.93.20.10"
+    volumes:
+     - ./data:/var/atlassian/application-data/jira
+     - ./config_files:/opt/atlassian/jira/atlassian-jira
+     - ./cacerts/cacerts:/opt/java/openjdk/lib/security/cacerts:ro
+    ports:
+     - '8880:8080'
+    restart: always
+    logging:
+     driver: "json-file"
+     options:
+       max-size: "5m"
+       max-file: "10"
+#networks:
+#  postgres11_13:
+#    external: true
+#    name: postgres11_13
+
+
